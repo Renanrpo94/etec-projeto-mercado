d710272aaad2b90467f413d04d373cb2ff4443a8
Renan Pereira <renan.rpo@hotmail.com>
2023-05-28 23:19:28 -0300

    Criando arquivo query.sql

---
+++ query.sql
@@ -0,0 +1,36 @@
+show databases;
+
+create database mercado;
+
+use mercado;
+
+create table produto(
+	codProd int (6) not null,
+    descricao varchar (60),
+    valor decimal (5,2),
+    validade date
+);
+
+desc produto;
+show tables;
+
+select * from produto;
+
+create table movto(
+	codProd int (6) not null,
+    tipo varchar (1) not null,
+    qtde decimal (7,2) not null,
+    datamov date not null
+);
+
+select * from movto;
+
+select * from produto order by codProd;
+select codProd, descricao, validade from produto order by validade;
+
+select * from produto where validade >= '2024-01-01' && validade < '2024-12-31' order by validade;
+
+
+
+
+
